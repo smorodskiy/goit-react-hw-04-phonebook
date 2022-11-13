062d35a4d5de383ee5e008505f5e1ff040c97aa2
Igor Smorodsky <ingerard@gmail.com>
2022-11-13 22:47:11 +0200

    to func comp

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Component } from 'react';
 
 // Check types of props
 import PropTypes from 'prop-types';
 import { Field } from 'components/base/Field/Field.styled';
 
-
-class Filter extends Component {
-  render() {
-    const { onInputFilter } = this.props;
-    return (
-      <>
-        <p>Find contacts by name</p>
-        <Field type="text" onChange={onInputFilter} />
-      </>
-    );
-  }
-}
-
-export { Filter };
+export const Filter = ({ onInputFilter }) => {
+  return (
+    <>
+      <p>Find contacts by name</p>
+      <Field type="text" onChange={onInputFilter} />
+    </>
+  );
+};
 
 // Types
 Filter.propTypes = {
